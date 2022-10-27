21a2748c91b81c96f2207d01b607504a4fe0ebaf
chewii <116803464+chewii@users.noreply.github.com>
2022-10-27 21:18:45 +0800

    Add files via upload

---
+++ choice.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="bootstrap/bootstrap.min.css" > 
+    <script src="bootstrap/f5f38fa04f.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="images/osoicon.png"></link>
+    <!-- Linking CSS File index.css -->
+
+    <link rel="stylesheet" href="choice.css">
+
+    <title>Choose An Account</title>
+</head>
+
+<body>
+				<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #fbee9b">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+    <a class="navbar-brand" href="index.php">
+    				<img src="images/osoicon.png" alt="" width="100" height="40">
+    </a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto my-2 my-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Back To Home</a>
+        </li>        
+          <li class="nav-item">
+          <a class="nav-link" href="#abtsec">About Us</a>
+          <li class="nav-item">
+          <a class="nav-link" href="#contactsec">Contact Us</a>              
+          </li>
+      </ul>
+    </div>    
+</nav>
+
+    <div class="container-fluid">
+        <h1 class="text-center mt-5 display-8 fw-bold">Login your <span class="theme-text"> Account</span></h1>
+        <hr class="mx-auto mb-1 w-25">
+        <div class="row mb-5">
+            <div class="col-12 col-sm-6 col-md-3 m-auto">
+                <!-- card starts here -->
+                <a href="user/login.php" class="redirect">
+                <div class="card shadow">
+                    <img src="images/userchoice.svg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h3 class="text-center">User Account</h3>
+                        <hr class="mx-auto w-75">
+                        <p class="textbox">
+                            For the people who  are interested to avail the ultimate Online Service Outlet experience.
+                        </p>
+                    </div>
+                </div>
+                </a>
+                <!-- card ends here -->
+            </div>
+            <!-- col ends here -->
+            <div class="col-12 col-sm-6 col-md-3 m-auto">
+                <!-- card starts here -->
+                <a href="admin/login.php" class="redirect">
+                <div class="card shadow">
+                    <img src="images/employeechoice.svg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h3 class="text-center">Employee Account</h3>
+                        <hr class="mx-auto w-75">
+                        <p class="textbox">
+                            For the hardworking and trustworthy slave workers of Online Service Outlet.
+                        </p>
+                    </div>
+                </div>
+                <!-- card ends here -->
+                </a>
+            </div>
+            <!-- col ends here -->
+            
+        </div>
+    </div>
+    
+    <!-- footer -->
+    <footer>
+      <div class="container">
+        <div class="wrapper">
+          <div class="footer-widget">
+            <a href="#">
+              <img src="images/osoicon.png" width="300" height="200" class="logo" />
+              <ul class="miniabt">
+              <li id="abtsec"><a>Online Service Outlet offers service in terms of online document transactions, online booking or appointment, online registrations, photocopy, print of documents or pictures, rush ID, with internet café, etc.</a></li>
+              </ul>
+            </a>
+          </div>
+          <div class="footer-widget">
+            <h6>Quick Links</h6>
+            <ul class="links">
+              <li><a href="osoindex.html">home</a></li>
+              <li><a href="#aboutsec">about us</a></li>
+              <li><a href="osoindex.html">services</a></li>
+              <li><a href="#contactsec">contact us</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget" id="contactsec">
+            <h6>Contact Us</h6>
+            <ul class="links">
+              <li><i class="fab fa-facebook"></i><a href="#">        OSOfficial</a></li>
+              <li><i class="fab fa-facebook-messenger"></i><a href="#">         OSOPH</a></li>
+              <li><i class="fas fa-phone"></i><a href="#">     09172232432 / (023) 263-777</a></li>
+              <li><i class="fas fa-map-marker-alt"></i><a href="#">       Magallanes St. Cavite, City</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget">
+            <h6>Help &amp; Support</h6>
+            <ul class="links">
+              <li><a href="#">support center</a></li>
+              <li><a href="#">live chat</a></li>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">terms &amp; conditions</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="copyright-wrapper">
+          <p>
+            Online Service Outlet © 2022
+          </p>
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="js/bootstrap.bundle.min.js" ></script>   
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>OSO | ONLINE SERVICE OUTLET</title>
+				<link rel="stylesheet" href="bootstrap/bootstrap.min.css" > 
+				<script src="bootstrap/f5f38fa04f.js" crossorigin="anonymous"></script>
+				<link rel="stylesheet" href="style.css"></link>
+				<link rel="shortcut icon" href="images/osoicon.png"></link>
+</head>
+<body>
+				<!-- Navigation bar start -->
+				<nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: #fbee9b">
+  <div class="container">								
+  				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+    <a class="navbar-brand" href="index.php">
+    				<img src="images/osoicon.png" alt="" width="100" height="40">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav ml-auto my-2 my-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active navbar-right" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="choice.php">Login/SignUp</a>
+        </li>        
+          <li class="nav-item">
+          <a class="nav-link" href="#aboutsec">About Us</a>
+          <li class="nav-item">
+          <a class="nav-link" href="#servicessect">Services Offered</a>
+          <li class="nav-item">
+          <a class="nav-link" href="#contactsec">Contact Us</a>              
+          </li>
+      </ul>
+    </div>    
+</nav>
+
+<!-- navigation bar ends -->
+
+<!-- main body start -->
+				<div class="main-index" id="home">
+								<div class="banner">																																														
+								<div class="bannertext">
+												<h1>Online Service Outlet</h1>
+												<p>A single outlet for all your needs</p>
+												<div class="button">
+																<a href="registerpage.html"><span></span>Read More</a>										
+												</div>
+								</div>
+				</div>
+
+<!-- services section starts  -->
+						<h2 class="section-heading" id="servicessect">Our Services</h2>
+<section class="servicesec">
+      <div class="row">
+      </div>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fa fa-print"></i>
+            </div>
+            <h3>Print</h3>
+            <p>
+              We offer quality printing with our superspendacular ultratastic paper.
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fa fa-image"></i>
+            </div>
+            <h3>Photo Print</h3>
+            <p>
+              A picture speaks a thousand words, and if you choose to print many photos that will give us thousands of money too<3
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fa fa-images"></i>
+            </div>
+            <h3>Tarpaulin/Poster Print</h3>
+            <p>
+              Have some corrupt politician that you wanna promote and slander their face? Got you!
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fas fa-birthday-cake"></i>
+            </div>
+            <h3>Invitation Cards</h3>
+            <p>
+              Our invitation cards will make you feel special that you would even want to invite yourself
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fas fa-tshirt"></i>
+            </div>
+            <h3>Customised (T-Shirt, Mugs, Giveaways, etc)</h3>
+            <p>
+              Special discount for those couples who want customized memorabilia, as long as they will break up instantly after.
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fas fa-id-badge"></i>
+            </div>
+            <h3>Laminate</h3>
+            <p>
+              Lamination so strong you can also use for cocaine use
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fas fa-tools"></i>
+            </div>
+            <h3>Repair</h3>
+            <p>
+              We can fix anything except your life.
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fas fa-book-open"></i>
+            </div>
+            <h3>Bookings & Appointment</h3>
+            <p>
+              We highly value Filipino time so expect to be always late every time.
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="icon-wrapper">
+              <i class="fas fa-hand-holding"></i>
+            </div>
+            <h3>Government Assistance</h3>
+            <p>
+              Because even the government can't provide it.
+            </p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </section>
+    
+    
+    
+    <!-- about us section -->
+    <div class="about-section" id="aboutsec">
+        <div class="inner-container">
+            <h1>About Us</h1>
+            <p class="text">
+                Online Service Outlet offers service in terms of online document transactions, online booking or appointment, online registrations, photocopy, print of documents or pictures, rush ID, with internet café, etc. The purpose of the business is to help our target customers to be more efficient in getting their needs in terms of online transactions of the documents like job documents, school and business related files, etc.
+            </p>
+            <div class="skills">
+                <span>We are happy to serve you!</span>
+            </div>
+        </div>
+    </div>
+    
+    <!-- footer -->
+    <footer>
+      <div class="container">
+        <div class="wrapper">
+          <div class="footer-widget">
+            <a href="#">
+              <img src="images/osoicon.png" width="300" height="200" class="logo" />
+            </a>
+          </div>
+          <div class="footer-widget">
+            <h6>Quick Links</h6>
+            <ul class="links">
+              <li><a href="#home">home</a></li>
+              <li><a href="#aboutsec">about us</a></li>
+              <li><a href="#servicessect">services</a></li>
+              <li><a href="#contactsec">contact us</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget" id="contactsec">
+            <h6>Contact Us</h6>
+            <ul class="links">
+              <li><i class="fab fa-facebook"></i><a href="#">        OSOfficial</a></li>
+              <li><i class="fab fa-facebook-messenger"></i><a href="#">         OSOPH</a></li>
+              <li><i class="fas fa-phone"></i><a href="#">     09172232432 / (023) 263-777</a></li>
+              <li><i class="fas fa-map-marker-alt"></i><a href="#">       Magallanes St. Cavite, City</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget">
+            <h6>Help &amp; Support</h6>
+            <ul class="links">
+              <li><a href="#">support center</a></li>
+              <li><a href="#">live chat</a></li>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">terms &amp; conditions</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="copyright-wrapper">
+          <p>
+            Online Service Outlet © 2022
+          </p>
+        </div>
+      </div>
+    </footer>
+
+<script src="js/bootstrap.bundle.min.js" ></script>
+
+</body>
+</html>
